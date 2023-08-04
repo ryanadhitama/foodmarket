b146603cc9a23f2c49b5877f051b04be3845a7f4
ryanadhitama <ryanadhitama2@gmail.com>
2023-08-04 23:02:19 +0800

    reorder component

--- src/components/molecules/index.tsx
+++ src/components/molecules/index.tsx
@@ -1,8 +1,8 @@
-export { default as Header } from './Header';
-export { default as FoodCard } from './FoodCard';
-export { default as HomeTabSection } from './HomeTabSection';
-export { default as HomeProfile } from './HomeProfile';
-export { default as Rating } from './Rating';
 export { default as BottomNavigator } from './BottomNavigator';
 export { default as EmptyOrder } from './EmptyOrder';
+export { default as FoodCard } from './FoodCard';
+export { default as Header } from './Header';
+export { default as HomeProfile } from './HomeProfile';
+export { default as HomeTabSection } from './HomeTabSection';
 export { default as ProfileTabSection } from './ProfileTabSection';
+export { default as Rating } from './Rating';
