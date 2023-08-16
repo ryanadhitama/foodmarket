dd09cfd3482364b5481cc42e20ea8781114530c6
ryanadhitama <ryanadhitama2@gmail.com>
2023-08-16 21:45:43 +0800

    signup

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Foodmarket
 
-| Splash Screen | Sign In  | Sign Up  |
-| ------------- | ------- | --------------- |
+| Splash Screen                                                                                                                | Sign In                                                                                                               | Sign Up                                                                                                               |
+| ---------------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------- | --------------------------------------------------------------------------------------------------------------------- |
 | <img src="https://raw.githubusercontent.com/ryanadhitama/foodmarket/master/screenshot/01.splash-screen.png" alt="drawing" /> | <img src="https://raw.githubusercontent.com/ryanadhitama/foodmarket/master/screenshot/02.signin.png" alt="drawing" /> | <img src="https://raw.githubusercontent.com/ryanadhitama/foodmarket/master/screenshot/03.signup.png" alt="drawing" /> |
--- src/redux/action/auth.ts
+++ src/redux/action/auth.ts
@@ -19,3 +19,43 @@ export const signInAction = (form: any, navigation: any) => (dispatch: any) => {
       showMessage(err?.response?.data?.data?.message || 'Error login', 'danger');
     });
 };
+
+export const signUpAction =
+  (dataRegister: any, photoReducer: any, navigation: any) => (dispatch: any) => {
+    Axios.post(`${API_HOST.url}/register`, dataRegister)
+      .then((res) => {
+        const token = `${res.data.data.token_type} ${res.data.data.access_token}`;
+        const profile = res.data.data.user;
+
+        storeData('token', { value: token });
+
+        if (photoReducer.isUploadPhoto) {
+          const photoForUpload = new FormData();
+          photoForUpload.append('file', photoReducer);
+
+          Axios.post(`${API_HOST.url}/user/photo`, photoForUpload, {
+            headers: {
+              Authorization: token,
+              'Content-Type': 'multipart/form-data'
+            }
+          })
+            .then((resUpload) => {
+              profile.profile_photo_url = `${API_HOST.storage}/${resUpload.data.data[0]}`;
+              storeData('userProfile', profile);
+              navigation.reset({ index: 0, routes: [{ name: 'SuccessSignUp' }] });
+            })
+            .catch((err) => {
+              showMessage(err?.response?.message || 'Uplaod photo tidak berhasil', 'danger');
+              navigation.reset({ index: 0, routes: [{ name: 'SuccessSignup' }] });
+            });
+        } else {
+          storeData('userProfile', profile);
+          navigation.reset({ index: 0, routes: [{ name: 'SuccessSignup' }] });
+        }
+        dispatch(setLoading(false));
+      })
+      .catch((err) => {
+        dispatch(setLoading(false));
+        showMessage(err?.response?.data?.message, 'danger');
+      });
+  };
